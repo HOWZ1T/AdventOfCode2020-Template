e75ed7879bbe97c1a04ae5bfd39766643f1b9937
HOWZ1T <dylan.d.randall@gmail.com>
2020-12-01 14:00:47 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![advent of code 2020 banner](aoc2020.png)
+
+# Advent Of Code 2020
+A simple interface for the advent of code 2020.
+Allows you to select which problem to run using the run problem drop down.
+
+Uses bootstrap for a simple UI.
+
+For an example please see: [day1.js](day1.js) and [index.html](index.html)
---
+++ day1.js
@@ -0,0 +1,15 @@
+function day1_part1() {
+    let rawInput = document.getElementById("textareaInput").value;
+    let outputElement = document.getElementById("textareaOutput");
+
+    if (rawInput === "") {return;}
+    outputElement.value = "Todo: Write your own solution";
+}
+
+function day1_part2() {
+    let rawInput = document.getElementById("textareaInput").value;
+    let outputElement = document.getElementById("textareaOutput");
+
+    if (rawInput === "") {return;}
+    outputElement.value = "Todo: Write your own solution";
+}
